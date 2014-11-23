f409c34392ea6d9bb174efe8525d9bb352036942
arthur van schravendijk <arthur_vanschravendijk@hotmail.com>
2014-11-23 14:28:08 +0100

    parse reset pass function

--- cloud/main.js
+++ cloud/main.js
@@ -8,10 +8,28 @@ Parse.Cloud.define("hello", function(request, response) {
 var Mandrill = require('mandrill');
 Mandrill.initialize('9ea9i9si1Ps6Z55VpUbT4w');
 
+
+// 			Parse.Cloud.run('sendNewPassword', { email: emailAdress, password: newPassword }, {
+// 				success: function(succes) {
+// 					alert(succes);
+// 					myFunctions.disableLoader();
+// 				},
+// 					error: function(error) { 
+// 					alert(error);
+// 					myFunctions.disableLoader();
+// 				}
+// 			});
+//     	} else {
+//     		alert("There is no user registerd under this emailadress: " + emailAdress);				    		
+// 			myFunctions.disableLoader();
+//     	}
+// 	}
+// });
+
 Parse.Cloud.define("sendNewPassword", function(request, response) {
   Mandrill.sendEmail({
     message: {
-      text: "here is your new password" + request.params.password,
+      text: "Your new password is: " + request.params.password,
       subject: "new password",
       from_email: "no-reply@fidence.com",
       from_name: "fidence support",
